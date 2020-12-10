1ab55c3ec0bc2e37f476505e1fff392ef431d9ed
J. Clarke <justineclarkes@gmail.com>
2020-12-10 20:41:09 +0000

    Working through Mongo DB Methods

--- myApp.js
+++ myApp.js
@@ -1,26 +1,74 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const personSchema= new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+mongoose
+.connect('mongodb+srv://JC:admin@cluster0.5z3ag.mongodb.net/PROJECT0?retryWrites=true&w=majority', {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log(Error, err.message);
+});
 
 let Person;
+Person = mongoose.model("Person", personSchema);
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+const createAndSavePerson =(done) => {
+  
+  var testPerson = new Person({name:"Test", age:43, favoriteFoods: ["vodka", "air"]});
+  
+  testPerson.save((err, data) => {
+    if (err) 
+    return console.error(err);
+    done(null, data)
+});
 };
+
+var arrayOfPeople = 
+[
+  {name: "Test", age: 42, favoriteFoods: ["vodka", "air"]},
+  {name: "Test1", age: 43, favoriteFoods: ["vodka", "air"]},
+  {name: "Test2", age: 44, favoriteFoods: ["vodka", "air"]}
+];
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) 
+    console.log("This is an error saving " + " " +  err);
+    done(null, data);
+  })
 };
+const Database = require("@replit/database")
+const db = new Database()
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.findOne({name: personName}, (err, data) => {
+      if(err) return console.log(err);
+      done(null, data);
+    }); 
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food},  (err,data) => {
+    if (err) return console.log(err);
+    done (null, data);
+  })
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({_id: personId}, (err,data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  }); 
 };
 
 const findEditThenSave = (personId, done) => {
@@ -53,7 +101,19 @@ const queryChain = (done) => {
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://JC:<password>@cluster0.5z3ag.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
  */
+
+
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
